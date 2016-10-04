4f6329805cf5fcd4b4aeb59357bc6a7f48135737
rafael <rafael.adel20@gmail.com>
2016-10-04 04:26:35 +0200

    fixed recursion

--- closest_pair/closest_pair.cpp
+++ closest_pair/closest_pair.cpp
@@ -20,19 +20,17 @@ struct Point {
      }
 };
 
-template<std::size_t SIZE>
-using p_iterator = typename std::array<Point, SIZE>::iterator;
+using p_iterator = std::vector<Point>::iterator;
 
 template<std::size_t SIZE>
-using p_iterators_array = std::array<typename std::array<Point, SIZE>::iterator, SIZE>;
+using p_iterators_array = std::array<std::vector<Point>::iterator, SIZE>;
 
-void initialize_points(std::array<Point, 10> &points) {
+void initialize_points(std::vector<Point> &points) {
     double x, y;
-    int i = 0;
     char c;
     std::ifstream infile("./points.txt");
-    while((infile >> x >> c >> y) && (c == ',') && (i < 11)) {
-        points[i++] = Point(x, y);
+    while((infile >> x >> c >> y) && (c == ',')) {
+        points.push_back(Point(x, y));
     }
 }
 
@@ -48,7 +46,7 @@ p_iterators_array<2> eucledian_closest(T &points, int size) {
     for(int i = 0; i < size - 1; i++){
       for(int j = i + 1; j < size; j++) {
         distance = calculate_distance(points[i], points[j]);
-        if(distance < closest_distance && distance != 0) {
+        if(distance < closest_distance ) {
           closest_distance = distance;
           closest_arr[0] = points + i;
           closest_arr[1] = points + j;
@@ -58,16 +56,15 @@ p_iterators_array<2> eucledian_closest(T &points, int size) {
     return closest_arr;
 }
 
-template<std::size_t SIZE>
-p_iterators_array<2> closest_split_pair(p_iterator<SIZE> points_iterator, p_iterators_array<2> &closest_side_pairs) {
-    std::vector<p_iterator<SIZE>> split_pairs;
+p_iterators_array<2> closest_split_pair(p_iterator points_iterator, p_iterators_array<2> &closest_side_pairs, std::size_t size) {
+    std::vector<p_iterator> split_pairs;
     p_iterators_array<2> final_result;
     double closest_distance = DBL_MAX, distance = 0.0;
 
-    typename std::array<Point, SIZE>::iterator midpoint = points_iterator + (SIZE/2);
+    p_iterator midpoint = points_iterator + (size/2);
 
     //filtering points to only points in sigma-2sigma rectangle
-    for (size_t i = 0; i < SIZE; i++) {
+    for (size_t i = 0; i < size; i++) {
       if(std::abs(points_iterator[i].x_coordinate - midpoint->x_coordinate) < calculate_distance(*(closest_side_pairs[0]), *(closest_side_pairs[1]))){
           split_pairs.push_back(points_iterator + i);
       }
@@ -77,7 +74,7 @@ p_iterators_array<2> closest_split_pair(p_iterator<SIZE> points_iterator, p_iter
     for (size_t i = 0; i < split_pairs.size() - 1; i++) {
       for (size_t j = i+1; (j < 7) && (j < split_pairs.size()) ; j++) {
         distance = calculate_distance(*(split_pairs[i]), *(split_pairs[j]));
-        if(distance < closest_distance && distance != 0) {
+        if(distance < closest_distance ) {
           closest_distance = distance;
           final_result[0] = split_pairs[i];
           final_result[1] = split_pairs[j];
@@ -92,39 +89,35 @@ p_iterators_array<2> closest_split_pair(p_iterator<SIZE> points_iterator, p_iter
     return final_result;
 }
 
-template<std::size_t SIZE>
-p_iterators_array<2> closest_side_pair(p_iterator<SIZE> points_iterator, p_iterator<SIZE> x_arr_iterator, p_iterator<SIZE> y_arr_iterator) {
-    std::size_t delimeter = SIZE / 2 ;
+p_iterators_array<2> closest_side_pair(p_iterator points_iterator, p_iterator x_arr_iterator, p_iterator y_arr_iterator, std::size_t size) {
+    std::size_t delimeter = size / 2 ;
     if(delimeter <= 3) {
       return eucledian_closest(points_iterator, delimeter);
     }
     p_iterators_array<2> closest_left, closest_right, result;
 
-    closest_left = closest_side_pair<SIZE / 2>(points_iterator, x_arr_iterator, y_arr_iterator);
-    closest_right = closest_side_pair<SIZE / 2>(points_iterator + delimeter, x_arr_iterator + delimeter, y_arr_iterator + delimeter);
+    closest_left = closest_side_pair(points_iterator, x_arr_iterator, y_arr_iterator, delimeter);
+    closest_right = closest_side_pair(points_iterator + delimeter, x_arr_iterator + delimeter, y_arr_iterator + delimeter, delimeter);
 
     if(calculate_distance(*(closest_left.front()), *(closest_left.back())) < calculate_distance(*(closest_right.front()), *(closest_right.back()))) {
       result = closest_left;
     } else {
       result = closest_right;
     }
-    return closest_split_pair<SIZE>(points_iterator, result);
+    return closest_split_pair(points_iterator, result, size);
 }
-
-
 
 int main()
 {
-    std::array<Point, 10> points;
+    std::vector<Point> points;
     initialize_points(points);
 
-    std::array<Point, 10> x_p = points;
-    std::array<Point, 10> y_p = points;
+    std::vector<Point> x_p = points;
+    std::vector<Point> y_p = points;
     std::sort(x_p.begin(), x_p.end(), Point::sortByX);
     std::sort(y_p.begin(), y_p.end(), Point::sortByY);
 
-    p_iterators_array<2> closest_result = closest_side_pair<10>(points.begin(), x_p.begin(), y_p.begin());
-    closest_result = closest_split_pair<10>(points.begin(), closest_result);
+    p_iterators_array<2> closest_result = closest_side_pair(points.begin(), x_p.begin(), y_p.begin(), points.size());
     std::cout << "Closest pair are: " << std::endl;
     for(auto p: closest_result) {
         std::cout << p->x_coordinate << ", " << p->y_coordinate << std::endl;
